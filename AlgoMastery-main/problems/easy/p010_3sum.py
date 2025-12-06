716c5c62abe3104f3ec4f52372475fbb3bf26350
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:07:21 +0530

    3 sum

--- AlgoMastery-main/problems/easy/p010_3sum.py
+++ AlgoMastery-main/problems/easy/p010_3sum.py
@@ -14,13 +14,33 @@ Returns:
 """
 
 def three_sum(nums: list[int]) -> list[list[int]]:
-    """
-    3Sum.
-
-    Args:
-        nums (list[int]): List of integers (0-indexed)
-
-    Returns:
-        list[list[int]]: List of triplets that sum to zero
-    """
-    pass
+    triplets = []
+    nums.sort()
+    # if len(nums)<=2:
+    #     return [-1,-1,-1]
+    m={}
+    for i in range(0,len(nums)-3):
+        sum=0
+        if m.get(nums[i]) is None:
+            m[nums[i]]=i
+            start=i+1
+            end=len(nums)-1
+            sum+=nums[i]
+            while start<end:
+                temp_triplets = []
+                while start< end and nums[start]==nums[start+1]:
+                    start+=1
+                while start<end and nums[end]==nums[end-1]:
+                    end-=1
+                if start<end and (nums[start]+nums[end]+sum) == 0:
+                    temp_triplets.append(nums[i])
+                    temp_triplets.append(nums[start])
+                    temp_triplets.append(nums[end])
+                    start+=1
+                    end-=1
+                    triplets.append(temp_triplets)
+                elif start<end and (nums[start]+nums[end]-sum) > 0:
+                    end-=1
+                else:
+                    start+=1
+    return triplets
