a276e4cba3f73125047cbea0efd097990b77d861
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:08:34 +0530

    longest substring without repeating characters

--- AlgoMastery-main/problems/easy/p014_longest_substring_without_repeating_characters.py
+++ AlgoMastery-main/problems/easy/p014_longest_substring_without_repeating_characters.py
@@ -14,14 +14,20 @@ Returns:
     int: Length of longest substring without repeating characters
 """
 
-def longest_substring_without_repeating_characters(s: str) -> int:
-    """
-    Longest Substring Without Repeating Characters.
-
-    Args:
-        s (str): Input string (0-indexed)
-
-    Returns:
-        int: Length of longest substring without repeating characters
-    """
-    pass
+def longest_substring_without_repeating_characters(nums: str) -> int:
+    temp={}
+    start=0
+    end=0
+    maxlen=0
+    while end<len(nums):
+        if nums[end] not in temp:
+            temp[nums[end]]=1
+            maxlen=max(maxlen,end-start+1)
+            end+=1
+        else:
+            while start<end and nums[end] in temp:
+                temp[nums[start]]-=1
+                if temp[nums[start]]==0:
+                    temp.pop(nums[start])
+                start+=1
+    return maxlen
