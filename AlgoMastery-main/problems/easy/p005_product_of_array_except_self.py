81fb9262116781f7ba0117700daaa7ae08f9f78f
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:04:37 +0530

    product of array except self

--- AlgoMastery-main/problems/easy/p005_product_of_array_except_self.py
+++ AlgoMastery-main/problems/easy/p005_product_of_array_except_self.py
@@ -15,14 +15,22 @@ Returns:
 """
 
 def product_of_array_except_self(nums: list[int]) -> list[int]:
-    """
-    Product of Array Except Self.
-
-    Args:
-        nums (list[int]): List of integers (0-indexed)
-
-    Returns:
-        list[int]: Product of array except self
-    """
-    # TODO: implement
-    pass
+    count=0
+    prd=1
+    ind=-1
+    for i in range(len(nums)):
+        if nums[i]==0:
+            count+=1
+            ind=i
+        else:
+            prd*=nums[i]
+    if count>1:
+        return [0]*len(nums)
+    elif count==1:
+        nums=[0]*len(nums)
+        nums[ind]=prd
+        return nums
+    else:
+        for i in range(len(nums)):
+            nums[i]=int(prd/nums[i])
+        return nums
