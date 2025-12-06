e060a9b91608d21f5fdbba6e2c3deef2a084eb04
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:30:08 +0530

    valid anagram

--- AlgoMastery-main/problems/easy/p019_valid_anagram.py
+++ AlgoMastery-main/problems/easy/p019_valid_anagram.py
@@ -16,14 +16,13 @@ Returns:
 """
 
 def valid_anagram(s: str, t: str) -> bool:
-    """
-    Valid Anagram.
+    dict1={}
+    dict2={}
+    for i in range(len(s)):
+        dict1[s[i]]=dict1.get(s[i],0)+1
+    for i in range(len(t)):
+        dict2[t[i]]=dict2.get(t[i],0)+1
+    return dict1==dict2
 
-    Args:
-        s (str): First string (0-indexed)
-        t (str): Second string (0-indexed)
 
-    Returns:
-        bool: True if t is an anagram of s, False otherwise
-    """
-    pass
+
