bf50c25f5a97db4d76a89a7c8ccf5f6f3d3ff5f7
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 23:49:48 +0530

    course schedule ii

--- AlgoMastery-main/problems/medium/p037_course_schedule_ii.py
+++ AlgoMastery-main/problems/medium/p037_course_schedule_ii.py
@@ -16,16 +16,31 @@ Returns:
     list[int]: Order to take courses, or empty list if impossible
 """
 
-def course_schedule_ii(num_courses: int, prerequisites: list[list[int]]) -> list[int]:
-    """
-    Course Schedule II.
-
-    Args:
-        num_courses (int): Total number of courses (0-indexed)
-        prerequisites (list[list[int]]): List of prerequisite pairs (0-indexed)
-
-    Returns:
-        list[int]: Order to take courses, or empty list if impossible
-    """
-    # TODO: implement
-    pass
+def course_schedule_ii(v: int, edges: list[list[int]]) -> list[int]:
+        adj=[[] for _ in range(v)]
+        vis=[0 for _ in range(v)]
+        indegree=[0 for _ in range(v)]
+        for i in range(len(edges)):
+            adj[edges[i][1]].append(edges[i][0])
+        
+                
+        for i in range(v):
+            for child in adj[i]:
+                indegree[child]+=1
+        q=[]
+        for i in range(len(indegree)):
+            if indegree[i]==0:
+                q.append(i)
+                vis[i]=1
+        topo=[]
+        while len(q)!=0:
+            temp_node=q.pop(0)
+            topo.append(temp_node)
+            for child in adj[temp_node]:
+                indegree[child]-=1
+                if indegree[child]==0 and vis[child]==0:
+                    q.append(child)
+                    vis[child]=1
+        if len(topo) != v:
+            return []
+        return topo
