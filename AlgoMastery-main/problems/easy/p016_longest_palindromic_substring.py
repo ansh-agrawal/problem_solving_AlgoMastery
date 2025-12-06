4d2131f89e16ebe97962276639c710351e6d54c8
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:09:57 +0530

    longest palindromic substring

--- AlgoMastery-main/problems/easy/p016_longest_palindromic_substring.py
+++ AlgoMastery-main/problems/easy/p016_longest_palindromic_substring.py
@@ -13,15 +13,24 @@ Args:
 Returns:
     str: Longest palindromic substring
 """
+def longest_palin_length(left,right,s):
+    while left>=0 and right<len(s)  and s[left]==s[right]:
+        left-=1
+        right+=1
+    return left+1,right-1
 
 def longest_palindromic_substring(s: str) -> str:
-    """
-    Longest Palindromic Substring.
-
-    Args:
-        s (str): Input string (0-indexed)
-
-    Returns:
-        str: Longest palindromic substring
-    """
-    pass
+    max_len=0
+    max_ind1=-1
+    max_ind2=-1
+    for i in range(len(s)):
+        if len(s)%2==0:
+            l1,l2=longest_palin_length(i,i+1,s)
+        else:
+            l1,l2=longest_palin_length(i,i,s)
+        if l2-l1>max_len:
+            max_len=l2-l1+1
+            max_ind1=l1
+            max_ind2=l2
+    ans_str=s[max_ind1:max_ind2+1]
+    return ans_str
