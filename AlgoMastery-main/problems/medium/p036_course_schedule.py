9bb3155bcedd8471923ce34d5b9c001cd16e696e
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 23:49:17 +0530

    course schedule

--- AlgoMastery-main/problems/medium/p036_course_schedule.py
+++ AlgoMastery-main/problems/medium/p036_course_schedule.py
@@ -15,16 +15,30 @@ Returns:
     bool: True if possible to finish all courses, False otherwise
 """
 
-def course_schedule(num_courses: int, prerequisites: list[list[int]]) -> bool:
-    """
-    Course Schedule.
+def course_schedule(nc: int, pq: list[list[int]]) -> bool:
 
-    Args:
-        num_courses (int): Total number of courses (0-indexed)
-        prerequisites (list[list[int]]): List of prerequisite pairs (0-indexed)
-
-    Returns:
-        bool: True if possible to finish all courses, False otherwise
-    """
-    # TODO: implement
-    pass
+    adj=[[] for _ in range(nc)]
+    vis=[0 for _ in range(nc)]
+    indegree=[0 for _ in range(nc)]
+    for i in range(len(pq)):
+            adj[pq[i][1]].append(pq[i][0])
+    for i in range(nc):
+            for child in adj[i]:
+                indegree[child]+=1
+    q=[] 
+    topo=[]
+    for i in range(nc):
+            if indegree[i]==0 and vis[i]==0:
+                q.append(i)
+                vis[i]=1
+    while len(q)!=0:
+            temp_node=q.pop()
+            topo.append(temp_node)
+            for child in adj[temp_node]:
+                indegree[child]-=1
+                if indegree[child]==0:
+                    q.append(child)
+                    vis[child]=1
+    if len(topo) == nc:
+            return True
+    return False
