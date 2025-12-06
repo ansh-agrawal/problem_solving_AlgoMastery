9f2c64a0cc50f8b61921b93e9cfc5302d0f47f42
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 21:30:46 +0530

    reorganize string

--- AlgoMastery-main/problems/easy/p021_reorganize_string.py
+++ AlgoMastery-main/problems/easy/p021_reorganize_string.py
@@ -13,15 +13,25 @@ Args:
 Returns:
     str: A valid rearrangement or empty string if not possible
 """
+import heapq
 
 def reorganize_string(s: str) -> str:
-    """
-    Reorganize String.
+    dict1={}
+    for i in range(len(s)):
+        dict1[s[i]]=dict1.get(s[i],0)+1
+    
+    max_heap=[(-cnt,char) for char,cnt in dict1]
+    heapq.heapify(max_heap)
+    ans=""
+    while max_heap:
+        cnt,char=heapq.heappop(max_heap)
+        ans+=char
+        cnt+=1
+        if cnt<0:
+            heapq.heappush(cnt,char)
+    
+    for i in range(len(ans)-1):
+        if ans[i]==ans[i+1]:
+            return ""
+    return ans
 
-    Args:
-        s (str): The input string (0-indexed)
-
-    Returns:
-        str: A valid rearrangement or empty string if not possible
-    """
-    pass
