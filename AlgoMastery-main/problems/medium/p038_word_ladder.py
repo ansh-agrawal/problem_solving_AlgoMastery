133a579b25bef17d8c7ff0687e873e86622c1a50
Ansh Agrawal <ansh_agrawal@epam.com>
2025-12-06 23:50:43 +0530

    word ladder

--- AlgoMastery-main/problems/medium/p038_word_ladder.py
+++ AlgoMastery-main/problems/medium/p038_word_ladder.py
@@ -16,17 +16,22 @@ Returns:
     int: Length of shortest transformation sequence, or 0 if impossible
 """
 
-def word_ladder(begin_word: str, end_word: str, word_list: list[str]) -> int:
-    """
-    Word Ladder.
-
-    Args:
-        begin_word (str): Starting word (0-indexed)
-        end_word (str): Target word (0-indexed)
-        word_list (list[str]): List of allowed words (0-indexed)
-
-    Returns:
-        int: Length of shortest transformation sequence, or 0 if impossible
-    """
-    # TODO: implement
-    pass
+def word_ladder(beginWord: str, endWord: str, wordList: list[str]) -> int:
+        temp={}
+        q=[]
+        vis={}
+        for i in range(len(wordList)):
+            temp[wordList[i]]=i
+        q.append([1,beginWord])
+        while len(q)!=0:
+            [num_steps,word]=q.pop(0)
+            if word==endWord:
+                return num_steps
+            for i in range(len(word)):
+                for j in range(ord('a'),ord('z')+1):
+                    temp_word=word[:i]+chr(j)+word[i+1:]
+                    if temp_word in temp and temp_word not in vis:
+                        vis[temp_word]=1
+                        q.append([num_steps+1,temp_word])
+            
+        return 0
